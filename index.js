a55db12d337eb88c878e15714800761ace203d44
epicasino2 <rayjonathanbadua@gmail.com>
2023-06-26 20:02:53 -0700

    index.js initial code

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "company_db",
+  },
+  console.log(`Connected to the company_db database.`)
+);
