1c12294730b785b71592f32e3680c39faa064b8e
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-27 23:20:35 -0700

    Results class made w/ methods

--- index.js
+++ index.js
@@ -1,63 +1,34 @@
 const inquirer = require("inquirer");
 const questions = require("./lib/questions");
-const mysql = require("mysql2");
-const { printTable } = require("console-table-printer");
+const Results = require("./lib/Results");
 
-const viewDepartments = () => {
-  db.query(`SELECT * FROM department`, (err, results) => {
-    let tableContent = [];
-    const generateTable = () => {
-      results.forEach((result) => {
-        tableContent.push({ id: result.id, department_name: result.name });
-      });
-      printTable(tableContent);
-    };
-    generateTable();
-  });
-};
-
-const viewEmployees = () => {
-  db.query(`SELECT * FROM employee`, (err, results) => {
-    let tableContent = [];
-    const generateTable = () => {
-      results.forEach((result) => {
-        tableContent.push({
-          id: result.id,
-          first_name: result.first_name,
-          last_name: result.last_name,
-          role_id: result.role_id,
-          manager_id: result.manager_id,
-        });
-      });
-      printTable(tableContent);
-    };
-    generateTable();
-  });
-};
-
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "company_db",
-  },
-  console.log(`Connected to the company_db database.`)
-);
+const resultMethods = new Results();
 
 const selectionMenu = () => {
   inquirer.prompt(questions).then((response) => {
     switch (response.mainMenu) {
       case "View All Employees":
-        viewEmployees();
+        resultMethods.viewEmployees();
         break;
       case "View All Departments":
-        viewDepartments();
+        resultMethods.viewDepartments();
         break;
       case "View All Roles":
-        viewDepartments();
+        resultMethods.viewRoles();
         break;
-    }
+      case "Create New Department":
+        resultMethods.createDepartment();
+        break;
+      case "Create New Role":
+        resultMethods.createRole();
+        break;
+      case "Create New Employee":
+        resultMethods.createEmployee();
+        break;
+      case "Update Employee":
+        resultMethods.updateEmployee();
+        break;
+    };
   });
 };
 
